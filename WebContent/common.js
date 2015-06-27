9aa7e6f2fd58e3a77386c676016ab1805489c8ce
erpsarang <sangyeul.han@gmail.com>
2015-06-27 19:13:46 +0900

    4th commit
    4th video code sample

---
+++ WebContent/common.js
@@ -0,0 +1,37 @@
+jQuery.sap.declare("sap.demo.cart.common");
+
+sap.demo.cart.common = {
+		
+		method1: function() {
+			
+			alert('metho1 called');
+			
+		},
+		
+		addToCart: function() {
+			
+			alert('add to cart called');
+			
+			
+		},
+		
+		cartPress: function(evt,obj) {
+			
+			var router = sap.ui.core.UIComponent.getRouterFor(obj);
+			
+			var list = sap.ui.getCore().byId("slistId");
+			
+			var selected = list.getSelectedItem();
+			
+			if(selected != null) {
+				
+				list.setSelectedItem(selected, false);
+				
+			}
+			
+			router.navTo("Cart");
+			
+			
+		}
+		
+}
